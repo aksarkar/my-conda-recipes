613b90437fe4378a28362d767800d1a32701a50a
Abhishek Sarkar <aksarkar@alum.mit.edu>
2019-05-09 14:59:11 -0500

    Add r-mashr

---
+++ r-mashr/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
+  $R CMD INSTALL --build .
+else
+    exit 1
+fi
